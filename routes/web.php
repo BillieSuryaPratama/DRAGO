a9fe12a613da4f5dfca60c404d43d853f18ff60a
thatsmeion <qurrotuaini1357@gmail.com>
2025-04-21 20:39:03 +0700

    PerubahanRoute1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PasswordController;
+use App\Http\Controllers\PemilikController;
+use App\Http\Controllers\PetaniController;
+use App\Http\Controllers\DeteksiController;
 
+// Rute untuk landing page
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingpage');
 });
+
+// Rute untuk menampilkan form login (GET)
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+
+// Rute untuk memproses login (POST)
+Route::post('/login', [LoginController::class, 'login']);
+
+// Rute untuk Beranda Pemilik
+Route::get('/berandapemilik', [PemilikController::class, 'index'])->name('berandapemilik');
+
+// Rute untuk Beranda Petani
+Route::get('/berandapetani', [PetaniController::class, 'index'])->name('berandapetani');
+Route::get('/akunpetani', [PetaniController::class, 'akunpetani'])->name('akunpetani');
+Route::get('/deteksi', DeteksiController::class, 'index')->name('deteksi');
+Route::get('/tambahgambar', DeteksiController::class, 'unggah')->name('tambahgambar');
+
+
+
